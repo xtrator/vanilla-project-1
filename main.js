24d1cc99997af400e15de2b5451a16851ad088c2
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-12 10:03:30 -0500

    Add tailwind css to app

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { setupCounter } from "./components/utils.js/counter";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <h1>Hello World</h1>
+    <h1 class="text-2xl text-teal-500">Hello World</h1>
     ${todo()}
     <button id="counter"></button>
   </div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./main.js", "./**/*.{js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// "./src/**/*.{js,ts,jsx,tsx}",
