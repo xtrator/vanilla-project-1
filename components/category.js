9e3a75e96d1e71527c9819278d7bf0d3ad8dfcef
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-12 19:43:09 -0500

    add event listeners to categories ul

--- components/category.js
+++ components/category.js
@@ -3,9 +3,14 @@ import downChevronUrl from "/chevron-down.svg";
 
 export function category() {
   return /* HTML */ `
-    <div>
+    <div class="flex flex-col items-start pt-6 gap-10">
       <p
-        class="flex gap-2 px-4 py-2 cursor-pointer font-bold"
+        class="text-2xl text-blue-600 border border-blue-400 px-5 py-2 rounded-md"
+      >
+        Collection of Drinks
+      </p>
+      <p
+        class="flex gap-2 px-4 py-2 cursor-pointer font-bold "
         id="categories-title"
       >
         <img src="${rightChevronUrl}" id="all-categories-chevron" /> All
@@ -19,21 +24,32 @@ export function category() {
 export async function setupCategory() {
   let allCategoriesNode = document.querySelector("#categories-title");
   let categories = [];
-  categories = await fetch("http://localhost:3333/products")
+  categories = await fetch("http://localhost:3333/products/categories")
     .then((res) => res.json())
     .then((res) => res.categories)
     .catch((e) => console.error(e));
 
-  const listItemStyles = "flex gap-2 px-4 py-2 cursor-pointer font-thin";
+  function handleClick(e) {
+    e.stopPropagation();
+    let li = e.target;
+    console.log(li.innerText);
+  }
+
+  const listItemStyles =
+    "flex gap-2 px-4 py-2 cursor-pointer font-thin hover:text-lg";
   if (categories.length == 0) {
     categories = [
       /* HTML*/ `<li class="${listItemStyles}"><img src="${rightChevronUrl}"/>Failed to fetch</li>`,
     ];
   } else {
     categories = categories.map((category) => {
-      return /* HTML */ ` <li class="${listItemStyles}">
-        <img src="${rightChevronUrl}" />${category.name}
-      </li>`;
+      let li = document.createElement("li");
+      li.innerHTML = /* HTML */ `<img
+          src="${rightChevronUrl}"
+        />${category.name}`;
+      li.className = listItemStyles;
+      li.addEventListener("click", (e) => handleClick(e));
+      return li;
     });
   }
 
@@ -51,7 +67,7 @@ function toggleCategories(categories) {
     categoriesNode.innerHTML = /* HTML */ ``;
     allCategoriesChevronNode.src = rightChevronUrl;
   } else {
-    categoriesNode.innerHTML = /* HTML */ ` ${categories.join("")} `;
+    categoriesNode.append(...categories);
     allCategoriesChevronNode.src = downChevronUrl;
   }
 }
