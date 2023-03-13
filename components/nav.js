6ecb15215664d0af10c26699f594d05a4a4b2a36
Alexander Luna <alexfox_2000@hotmail.com>
2023-03-12 19:49:16 -0500

    minor style changes

--- components/nav.js
+++ components/nav.js
@@ -1,17 +1,17 @@
 import shopLogo from "/Logo.svg";
 
 export default function nav() {
-  return `
+  return /* HTML */ `
     <nav class="flex justify-between h-22 items-center px-4 py-6">
-      <div class="flex gap-3 items-end">
-        <img src="${shopLogo}" class="h-10" />
-        <h1 class="text-3xl">
-          Shopdrink
-        </h1>
+      <div>
+        <a href="/" class="flex gap-3 items-end"
+          ><img src="${shopLogo}" class="h-10" />
+          <h1 class="text-3xl">Shopdrink</h1></a
+        >
       </div>
-      <input 
-        type="text" 
-        placeholder="Your search..." 
+      <input
+        type="text"
+        placeholder="Your search..."
         class="bg-gray-100 rounded-full focus:outline-gray-200 px-11 py-2 w-full max-w-xl text-gray-600"
       />
       <button
@@ -20,5 +20,5 @@ export default function nav() {
         My cart
       </button>
     </nav>
-    `;
+  `;
 }
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import { category, setupCategory } from "./components/category";
 document.querySelector("#app").innerHTML = /* HTML */ `
   <div class="h-screen">
     ${nav()}
-    <div class="px-56 font-light">${category()}</div>
+    <div class="2xl:px-56 px-40 font-light">${category()}</div>
   </div>
 `;
 
